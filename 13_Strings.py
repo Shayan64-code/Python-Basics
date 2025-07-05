9584d9d32daa0615c8d84b09c0a9f93851deda54
Shayan64-code <shayanomar9858@gmail.com>
2025-07-05 16:01:07 +0500

    Update 13_Strings.py

--- 13_Strings.py
+++ 13_Strings.py
@@ -20,8 +20,15 @@
 #     print("It is not present")
 
 # Slicing String
-b = "Hello, World!"
-print(b[0:5])
-print(b[:5])    # from start to 5
-print(b[2:])    # from 2 to end
-print(b[-6:-1])   #in reverse order
+b = " Hello, World! "
+# print(b[0:5])
+# print(b[:5])    # from start to 5
+# print(b[2:])    # from 2 to end
+# print(b[-6:-1])   #in reverse order
+
+print(b.upper())
+print(b.lower())
+print(b.capitalize())
+print(b.split(","))
+print(b.strip())      #remove spaces from start and end
+print(b.replace("H","M"))
