d956b7b298a48ef0b4d7c56651dcd8b70c7d185f
Shayan64-code <shayanomar9858@gmail.com>
2025-07-28 00:23:42 +0500

    Update 17_f_Stringd.py

--- 17_f_Stringd.py
+++ 17_f_Stringd.py
@@ -1,4 +1,7 @@
-#print(f"{k} == {val}")                 #print the key and value between them
+# k = "first"       #or maybe dictionaries
+# val = "Last"
+
+# print(f"{k} == {val}")                 #print the key and value between them
 
 # def fun(**kwargs):
 #     for k, val in kwargs.items():
@@ -12,3 +15,17 @@
 #     print("Keyword arguments:", kwargs)
 
 # fun(1, 2, 3, a=4, b=5)
+
+# name = "Shayan"
+# country = "Pakistan"
+
+# txt = (f"My name is {name} and I live in {country}.")
+# print(txt)
+
+# txt2 = (f"My name is {{name}} and I live in {{country}}.")  #to print the exact code
+# print(txt2)
+
+price = 19.999999
+
+txt3 = (f"You can buy this in just {price:.2f}$.")
+print(txt3)
