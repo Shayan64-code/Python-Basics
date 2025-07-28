82f6bf958aa8a55e5f5e4168873eb7d5a466baa9
Shayan64-code <shayanomar9858@gmail.com>
2025-07-28 08:25:05 +0500

    Create 20_Doc_String.py

---
+++ 20_Doc_String.py
@@ -0,0 +1,8 @@
+def ex(n):
+    '''This is doc string it is used to any function, method, class, module. It is used to determine that
+    what are the function etc purpose is. A doc string can only be made from starting line after declaring 
+    function etc.'''
+    print(n**2)
+
+ex(5)
+print(ex.__doc__)
