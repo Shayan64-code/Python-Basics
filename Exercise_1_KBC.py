b1daaa88af7bd7219a27774ebc99bbabf1fff36e
Shayan64-code <shayanomar9858@gmail.com>
2025-08-18 15:25:24 +0500

    Update KBC

--- Exercise_1_KBC.py
+++ Exercise_1_KBC.py
@@ -51,29 +51,78 @@
 #         print("This is Wrong")
 
 
-#Practice:
-KBC = [
-    {"question":"What is Capital of France: ","answer":"Paris"},
-    {"question":"What is 2+2: ","answer":"4"},
-    {"question":"What is the King of Fruits: ","answer":"Mango"}
-]
-
-money = 0
-for x in KBC:
-    print(x["question"])
-    ans = input("Give Your Answer: ")
-    if ans.strip().lower()==x["answer"].strip().lower():
-        print("Your answer is correct!!!")
-        money +=100                          #money/or every other variable cannot assign directly in print or
-        print("Money added Rs 100:", money)   #other like this money+=100.
-    else:
-        print("You are wrong")
-        money -=100
-        print("Money deducted Rs 100:", money) 
-print("Money Taken Home:",money)
-
-# questions = [
-#     ["What is Capital of France: ","Paris"],
-#     ["What is 2+2: ","4"],
-#     ["What is the King of Fruits: ","Mango"]
+# #Practice:
+# KBC = [
+#     {"question":"What is Capital of France: ","answer":"Paris"},
+#     {"question":"What is 2+2: ","answer":"4"},
+#     {"question":"What is the King of Fruits: ","answer":"Mango"}
 # ]
+
+# money = 0
+# for x in KBC:
+#     print(x["question"])
+#     ans = input("Give Your Answer: ")
+#     if ans.strip().lower()==x["answer"].strip().lower():
+#         print("Your answer is correct!!!")
+#         money +=100                          #money/or every other variable cannot assign directly in print or
+#         print("Money added Rs 100:", money)   #other like this money+=100.
+#     else:
+#         print("You are wrong")
+#         money -=100
+#         print("Money deducted Rs 100:", money) 
+# print("Money Taken Home:",money)
+
+
+questions = [
+    ["What is Capital of France: ","Paris","Italy","German","France","a"],
+    ["What is 2+2: ","5","11","4","12", "c"],
+    ["What is the King of Fruits: ","Apple","Mango","Banana","Kiwi","b"],
+    ["What is my name: ","Ahmed","Farooq","Ali","Shayan","d"]
+]
+levels = [10000, 20000, 40000, 80000, 160000, 320000, 640000, 1280000, 2560000, 5120000, 10240000, 20480000]
+
+#Example with List in List:
+
+# money = 0
+# for i in range(0, len(questions)):
+#     question = questions[i]
+#     #print(questions[i])
+#     print(f"Question for Rs. {levels[i]}")
+#     print(f"Question is: {question[0]}")
+#     print(f"a. {question[1]}        b. {question[2]}")
+#     print(f"c. {question[3]}        d. {question[4]}")
+
+#     reply = input("Your Answer: ")
+#     if reply == question[-1]:
+#         print(f"Your answer is correct, Added Rs:{levels[i]}")
+#         money = int(levels[i])
+#     else:
+#         print(f"Your answer is wrong")
+#         break
+# print(f"Money you earned: Rs {money}")
+
+###Same but little changes.
+money = 0
+for i in range(0, len(questions)):
+    question = questions[i]
+    #print(questions[i])
+    print(f"Question for Rs. {levels[i]}")
+    print(f"Question is: {question[0]}")
+    print(f"a. {question[1]}        b. {question[2]}")
+    print(f"c. {question[3]}        d. {question[4]}")
+
+    reply = input("Your Answer: ")
+    if reply == question[-1]:
+        print(f"Your answer is correct, Added Rs:{levels[i]}")
+        if i == 0:
+            money = int(levels[0])
+        elif i == 3:
+            money = int(levels[3])
+        elif i == 7:
+            money = int(levels[7])
+        elif i == 11:
+            money = int(levels[11])
+    else:
+        print(f"Your answer is wrong")
+        break
+print(f"Money you earned: Rs {money}")
