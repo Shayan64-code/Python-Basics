220577576b5b0504186610a424aef8f8def6f09d
Shayan64-code <shayanomar9858@gmail.com>
2025-08-22 01:33:00 +0500

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,6 @@
-try:
-#     num = int(input("Write the number for multiplication table: "))
-#     a = [6, 8]
-#     print(a[num])
-# except ValueError:                     #This will handle specific error in a specific way.
-#     print("This is Value Error.")     #if we put input instead of a int.
-# except IndexError:
-#     print("This is an Index Error.")
+def fun(s):
+#     s += 'GFG'  
+#     print("Inside Function", s)
+
+# s = "I love Geeksforgeeks"
+# fun(s)
